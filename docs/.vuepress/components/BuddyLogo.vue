6639bacc9bdc439f929c7d6fa86df3631649da22
CNine <cnine229@gmail.com>
2018-10-23 12:25:36 +0800

    Logo可点击弹出提示

--- docs/.vuepress/components/BuddyLogo.vue
+++ docs/.vuepress/components/BuddyLogo.vue
@@ -1,6 +1,6 @@
 <template>
-  <img v-if="src" class="buddy-logo" :src="src" :alt="who" :title="who" />
-  <span v-else="src" class="buddy-logo text-logo" :alt="who" :title="who">{{ who }}</span>
+  <img v-if="src" @click="handleClick" class="buddy-logo" :src="src" :alt="who" :title="who" />
+  <span v-else="src" @click="handleClick" class="buddy-logo text-logo" :alt="who" :title="who">{{ who }}</span>
 </template>
 
 <script>
@@ -14,7 +14,15 @@ export default {
     }
   },
   methods: {
-    GetBuddyLogo
+    GetBuddyLogo,
+    handleClick() {
+      let root = document.body
+      let el = document.createElement('div')
+      el.innerText = this.who
+      el.setAttribute('class', 'who-poper')
+      root.appendChild(el)
+      let t = setTimeout(() => { root.removeChild(el) || clearTimeout(t) }, 5000)
+    }
   }
 }
 </script>
@@ -22,9 +30,10 @@ export default {
 <style lang="stylus">
 .buddy-logo
   height 32px
-  padding 8px
-  margin 8px
+  padding 6px
+  margin 4px
   transition all .3s
+  cursor pointer
 img.buddy-logo
   vertical-align middle
 .text-logo
@@ -36,7 +45,31 @@ img.buddy-logo
   .buddy-logo
     height 18px
     padding 4px
-    margin 4px
   .text-logo
     font-weight 300
+.who-poper
+  position fixed
+  z-index 666666
+  width 120px
+  text-align center
+  padding 10px 20px
+  left 50%
+  margin-left -60px
+  background #282c3475
+  color #fff
+  border-radius 5px
+  animation upup 5s 1
+@keyframes upup
+  0% 
+    bottom 50px
+    opacity 0
+  5%
+    bottom 160px
+    opacity 1
+  95%
+    bottom 160px
+    opacity 1
+  100%
+    bottom 50px
+    opacity 0
 </style>
